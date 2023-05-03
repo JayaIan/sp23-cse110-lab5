7faec8db1fda27f5c7b917488bf0533fff7e7258
ijayacha <ijayacha@ucsd.edu>
2023-05-03 16:31:00 -0700

    select-audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,27 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectSound = document.getElementById("horn-select");
+  const imageArea = document.querySelector("#expose img");
+  const soundSource = document.getElementsByClassName("hidden");
+  // console.log(soundSource);
+  selectSound.addEventListener("change", (event) => {
+    //console.log(event.target.value);
+    if (event.target.value == "air-horn") {
+      imageArea.src = "assets/images/air-horn.svg";
+      soundSource.src = "assets/audio/air-horn.mp3";
+    }
+    else if (event.target.value == "car-horn") {
+      imageArea.src = "assets/images/car-horn.svg";
+      soundSource.src = "assets/audio/car-horn.mp3";
+    }
+    else if (event.target.value == "party-horn") {
+      imageArea.src = "assets/images/party-horn.svg";
+      soundSource.src = "assets/audio/party-horn.mp3";
+    }
+    else {
+      imageArea.src = "assets/images/no-image.png";
+      soundSource.src = "";
+    }
+  });
 }
