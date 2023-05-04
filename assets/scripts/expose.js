e0da2b0b282dfcbb2717db549ed918fb98e3f5ec
ijayacha <ijayacha@ucsd.edu>
2023-05-03 17:07:09 -0700

    play-sounds

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,33 +6,36 @@ function init() {
   // Change image and audio based on user selection
   const selectSound = document.getElementById("horn-select");
   const imageArea = document.querySelector("#expose img");
-  const soundSource = document.getElementsByClassName("hidden");
+  const soundSource = document.querySelector(".hidden");
+  var party = false; 
   selectSound.addEventListener("change", (event) => {
     if (event.target.value == "air-horn") {
       imageArea.src = "assets/images/air-horn.svg";
       soundSource.src = "assets/audio/air-horn.mp3";
+      party = false; 
     }
     else if (event.target.value == "car-horn") {
       imageArea.src = "assets/images/car-horn.svg";
       soundSource.src = "assets/audio/car-horn.mp3";
+      party = false; 
     }
     else if (event.target.value == "party-horn") {
       imageArea.src = "assets/images/party-horn.svg";
       soundSource.src = "assets/audio/party-horn.mp3";
+      party = true;
     }
     else {
       imageArea.src = "assets/images/no-image.png";
       soundSource.src = "";
+      party = false; 
     }
   });
 
   // Change volume based on user selection
   const soundVolume = document.getElementById("volume");
   const volumeImage = document.querySelector("div>img");
-  //console.log(volumeImage);
   soundVolume.addEventListener("input", (event) => {
     soundSource.volume = event.target.value/100;
-    console.log(soundSource.volume);
     if (event.target.value == 0) {
       volumeImage.src = "assets/icons/volume-level-0.svg";
     }
@@ -46,4 +49,14 @@ function init() {
       volumeImage.src = "assets/icons/volume-level-3.svg";
     }
   });
+
+    // Play sound on click
+    const playButton = document.querySelector("button");
+    const jsConfetti = new JSConfetti();
+    playButton.addEventListener("click", (event) => {
+      soundSource.play();
+      if (party) {
+        jsConfetti.addConfetti();
+      }
+    });
 }
